beef152f1da4aa3b4957aa515ac9ee27a9a37510
angelteeza26-a11y <angelteeza26@gmail.com>
2026-02-25 16:31:23 -0500

    Update robinhood.js

--- plugins/robinhood.js
+++ plugins/robinhood.js
@@ -3,44 +3,44 @@ var sourceName = "Robin Hood Library";
 var baseUrl = "https://mis-archivos-web.pages.dev/";
 
 function popularNovels(page) {
-return Promise.resolve([
-{
-name: "Verdant (Twoony)",
-path: "Ver-Two/chap_1.xhtml",
-cover: ""
-}
-]);
+  return Promise.resolve([
+    {
+      name: "Verdant (Twoony)",
+      path: "Ver-Two/chap_1.xhtml",
+      cover: ""
+    }
+  ]);
 }
 
 function parseNovel(novelPath) {
-return Promise.resolve({
-name: "Verdant (Twoony)",
-path: novelPath,
-chapters: [{ name: "Capítulo 1", path: novelPath }]
-});
+  return Promise.resolve({
+    name: "Verdant (Twoony)",
+    path: novelPath,
+    chapters: [{ name: "Capítulo 1", path: novelPath }]
+  });
 }
 
 function parseChapter(chapterPath) {
-return fetch(baseUrl + chapterPath)
-.then(function(res) { return res.text(); })
-.then(function(html) {
-var parts = html.split('<div class="calibreEbookContent">');
-var text = parts.length > 1 ? parts[1].split('<div class="calibreEbNavBottom">')[0] : "Error de carga";
-return {
-chapterName: "Capítulo 1",
-chapterText: text
-};
-});
+  return fetch(baseUrl + chapterPath)
+    .then(function(res) { return res.text(); })
+    .then(function(html) {
+      var parts = html.split('<div class="calibreEbookContent">');
+      var text = parts.length > 1 ? parts[1].split('<div class="calibreEbNavBottom">')[0] : "Error de carga";
+      return {
+        chapterName: "Capítulo 1",
+        chapterText: text
+      };
+    });
 }
 
 module.exports = {
-default: {
-id: sourceId,
-name: sourceName,
-site: baseUrl,
-version: "1.0.4",
-popularNovels: popularNovels,
-parseNovel: parseNovel,
-parseChapter: parseChapter
-}
+  default: {
+    id: sourceId,
+    name: sourceName,
+    site: baseUrl,
+    version: "1.0.4",
+    popularNovels: popularNovels,
+    parseNovel: parseNovel,
+    parseChapter: parseChapter
+  }
 };
