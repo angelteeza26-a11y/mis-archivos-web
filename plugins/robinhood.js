1352dc6db9df9054de1ae2f40a0995abf6275e95
angelteeza26-a11y <angelteeza26@gmail.com>
2026-02-25 09:01:04 -0500

    robinhood.js

--- plugins/robinhood.js
+++ plugins/robinhood.js
@@ -2,25 +2,45 @@ const sourceId = 123456;
 const sourceName = "Robin Hood Library";
 const baseUrl = "https://angelteeza26-a11y.github.io/mis-archivos-web/";
 
-async function popularNovels(page) {
-  return [
-    {
+const RobinHoodLibrary = {
+  id: "robinhood_lib",
+  name: sourceName,
+  icon: "https://m.media-amazon.com/images/S/compressed.photo.goodreads.com/books/1768245550i/246490524.jpg",
+  site: baseUrl,
+  version: "1.0.0",
+
+  popularNovels: async function(page) {
+    return [
+      {
+        name: "Verdant (Twoony)",
+        path: "Ver-Two/chap_1.xhtml",
+        cover: "https://m.media-amazon.com/images/S/compressed.photo.goodreads.com/books/1768245550i/246490524.jpg" 
+      }
+    ];
+  },
+
+  parseNovel: async function(novelPath) {
+    return {
       name: "Verdant (Twoony)",
-      link: "Ver-Two/chap_1.xhtml",
-      cover: "https://m.media-amazon.com/images/S/compressed.photo.goodreads.com/books/1768245550i/246490524.jpg" 
-    }
-  ];
-}
+      path: novelPath,
+      cover: "https://m.media-amazon.com/images/S/compressed.photo.goodreads.com/books/1768245550i/246490524.jpg",
+      chapters: [{ name: "Capítulo 1", path: novelPath }]
+    };
+  },
 
-async function parseChapter(chapterLink) {
-  const url = baseUrl + chapterLink;
-  const response = await fetch(url);
-  const html = await response.text();
+  parseChapter: async function(chapterPath) {
+    const url = baseUrl + chapterPath;
+    const response = await fetch(url);
+    const html = await response.text();
     
-  const chapterText = html.split('<div class="calibreEbookContent">')[1].split('<div class="calibreEbNavBottom">')[0];
+    // Tu lógica de limpieza de texto
+    const chapterText = html.split('<div class="calibreEbookContent">')[1].split('<div class="calibreEbNavBottom">')[0];
 
-  return {
-    chapterName: "Capítulo",
-    chapterText: chapterText,
-  };
-}
+    return {
+      chapterName: "Capítulo 1",
+      chapterText: chapterText,
+    };
+  }
+};
+
+export default RobinHoodLibrary;
